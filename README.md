264ad4c42567af862c4da0fdeb386cac87ee2c1c
龙奎 <kui.long@nascent.cn>
2025-07-20 00:43:03 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # account-book
+
+一个用于管理个人账本的开源项目。
+
+
+
+
+
+
+
+
+
+
+
+
+## License
+
+This project is licensed under the [Apache License 2.0](LICENSE).
+
+This means you can freely use, modify, and distribute this software, even commercially, as long as you include proper attribution.
---
+++ src/test/java/com/lonkui/book/accountbook/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonkui.book.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AccountBookApplication.class)
+class AccountBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
